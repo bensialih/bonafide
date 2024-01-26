2ff39774d1991ec26565da5e47ea8757276f9a5f
Abdel Hakim Bensiali <bensialih@gmail.com>
2024-01-26 21:22:47 +0000

    modular change to project structure

--- api/src/main.rs
+++ api/src/main.rs
@@ -7,16 +7,16 @@ mod models;
 mod endpoints;
 use endpoints::{root, basic, list_ips};
 
+const PORT: u16 = 3000;
 
 #[tokio::main]
 async fn main() {
+	println!("starting application on port {:?}", PORT);
 	let app = Router::new()
 		.route("/", get(root))
 		.route("/entry/", get(basic))
 		.route("/ips/", get(list_ips));
 
-	let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+	let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", PORT)).await.unwrap();
 	axum::serve(listener, app).await.unwrap();
 }
-
-
